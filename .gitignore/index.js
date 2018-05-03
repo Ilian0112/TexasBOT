5ebee9d256bd484c02077f108a7122417fc4286e
Ilian0112 <38587349+Ilian0112@users.noreply.github.com>
2018-05-03 17:11:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -437,4 +437,4 @@ bot.on("message", async function(message) {
     }
 });
 
-bot.login('NDM4NDI4NTEwMTMxMDYwNzQ2.Dct8Cw.-4JX-rfav9K8Vxm-_yTBQuQZAc4');
+bot.login('NDM4NDI4NTEwMTMxMDYwNzQ2.Dcy4Fg.SWuuJ8il3_eHBbNX2fXpmGddKUQ');
